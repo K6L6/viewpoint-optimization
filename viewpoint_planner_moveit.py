9e7c7ed1701075bead6fe61fbff176fffa9fab54
K6L6 <kelvin-raku@moegi.waseda.jp>
2020-07-21 19:24:37 +0900

    update initial position

--- viewpoint_planner_moveit.py
+++ viewpoint_planner_moveit.py
@@ -4,23 +4,18 @@ import cv2
 import numpy as np
 import socket
 import json
-import copy
-import math
 import sys
 from time import sleep
-import cPickle as pickle
 import ipdb
+import cPickle as pickle
 from six.moves import queue
 
 import rospy
+import tf
 import rospkg
 # import baxter_interface
-import moveit_commander
-import moveit_msgs.msg
-from std_msgs.msg import String
-from moveit_commander.conversions import pose_to_list
 from sensor_msgs.msg import Image
-from simulator_observer import GetImage#, MoveBaxter
+from simulator_observer import GetImage, MoveBaxter
 
 from gazebo_msgs.srv import (
     SpawnModel, 
@@ -94,8 +89,6 @@ rospy.init_node('test',anonymous=True)
 
 robot = moveit_commander.RobotCommander()
 scene = moveit_commander.PlanningSceneInterface()
-# limb = 'left'
-# Bax = MoveBaxter(limb)
 im = GetImage()
 
 HOST = '192.168.170.209'  # The server's hostname or IP address
@@ -117,6 +110,7 @@ starting_joint_angles = {'left_w0': 0.6699952259595108,
                             'left_e1': 1.9400238130755056,
                             'left_s0': -0.08000397926829805,
                             'left_s1': -0.9999781166910306}
+
 ## obtain initial position
 left_current_pose = group.get_current_pose(end_effector_link = 'left_gripper').pose
 print(left_current_pose)
@@ -132,26 +126,16 @@ group.go(wait=True)
 moveit_commander.roscpp_shutdown()
 
 sys.exit()
-# Bax._guarded_move_to_joint_position(starting_joint_angles)
-# init_pose = Pose()
-# init_pose.position.x, init_pose.position.y, init_pose.position.z, init_pose.orientation.x, init_pose.orientation.y, init_pose.orientation.z, init_pose.orientation.w = Bax.get_current_pose()
 
-## object to store movement of end-effector
-move = Pose()
-move.position.x, move.position.y, move.position.z, move.orientation.x, move.orientation.y, move.orientation.z, move.orientation.w = Bax.get_current_pose()
-move.position.x -= 0.2
+## Set initial orientation
 
-next_joint_angles = Bax.get_joint_angles(move)
-Bax._guarded_move_to_joint_position(next_joint_angles)
-move.position.z -=0.37
-next_joint_angles = Bax.get_joint_angles(move)
-Bax._guarded_move_to_joint_position(next_joint_angles)
-
+quaternion_0 = [move.orientation.x, move.orientation.y, move.orientation.z, move.orientation.w]
 offset = np.asarray([move.position.x, move.position.y, move.position.z]) - np.asarray([-0.2,0.0,0.0])
 
 observed_viewpoint = np.asarray(gazeboPose2GQN_VP([move.position.x, move.position.y, move.position.z],offset),dtype=np.float32)
 #observed_viewpoint = np.expand_dims(np.expand_dims(observed_viewpoint, axis=0), axis=0)
-
+print("start position:")
+print(move)
 #camera_distance = np.mean(np.linalg.norm(observed_viewpoint[:,:,:3],axis=2))
 #camera_position_z = np.mean(observed_viewpoint[:,:,1])
 camera_distance = np.mean(np.linalg.norm(observed_viewpoint[:3]))
@@ -190,28 +174,31 @@ while True:
     except socket.error:
         s.close()
         print("communication cut")
-        break
-         
-
-print(data_r.get())
+        break         
+    
 next_vp = data_r.get()
+print("new viewpoint received")
 x,y,z,yaw,pitch = next_vp
+
 camera_pos = [x,y,z]
-pose_x, pose_y, pose_z = GQN_VP2gazeboPose(camera_pos,offset)
+print("convert to Gazebo quaternion")
+pos_x, pos_y, pos_z = GQN_VP2gazeboPose(camera_pos,offset)
+# ipdb.set_trace()
+quaternion_yaw = [0,0,np.sin(yaw/2),np.cos(yaw/2)]
+quaternion_pitch = [0,np.sin(pitch/2),0,np.cos(pitch/2)]
 
+move.position.x = pos_x
+move.position.y = pos_y
+move.position.z = pos_z
 
+quaternion_1 = tf.transformations.quaternion_multiply(quaternion_0, quaternion_yaw)
+quaternion_1 = tf.transformations.quaternion_multiply(quaternion_1, quaternion_pitch)
+move.orientation.x, move.orientation.y, move.orientation.z, move.orientation.w = quaternion_1
 
-# quaternion_yaw = [0,0,np.sin(yaw/2),np.cos(yaw/2)]
-# quaternion_pitch = [0,np.sin(pitch/2),0,np.cos(pitch/2)]
-# move.position.x = pose_x
-# move.position.y = pose_y
-# move.position.z = pose_z
-# quaternion_1 = tf.transformations.quaternion_multiply(quaternion_0, quaternion_yaw)
-# quaternion_1 = tf.transformations.quaternion_multiply(quaternion_1, quaternion_pitch)
-# move.orientation.w, move.orientation.x, move.orientation.y, move.orientation.z = quaternion_1
-
-# joint_angles = Bax.get_joint_angles(move)
-# Bax._guarded_move_to_joint_position(joint_angles)
+print("new position:")
+print(move)
+joint_angles = Bax.get_joint_angles(move)
+Bax._guarded_move_to_joint_position(joint_angles)
 
 # print("variance:"+str(highest_var))
 # print("viewpoint data: "+str(highest_var_vp))
